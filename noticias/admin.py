26f55bf3f532e160d590df505507c33bc107548c
josemoreira2025071-spec <jose.moreira.2025071@my.istec.pt>
2026-04-02 12:56:56 +0000

    painel do admin

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Artigo, Comentario
 
-# Register your models here.
+# Isto faz com que os modelos apareçam no painel
+admin.site.register(Artigo)
+admin.site.register(Comentario)
